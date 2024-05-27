273f55124dd8f9e4213cd3f23a55a87a340ab95f
DiegoCorrea07 <diego.correa.cumbicus07@outlook.com>
2024-05-26 22:23:02 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Persona
+    {
+     
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
